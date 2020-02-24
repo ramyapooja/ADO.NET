fde18b4ec39370e9c48322df6b83cf4347a0456c
ramyapooja <ramyapooja666@gmail.com>
2020-02-24 17:21:53 +0530

    24-2-2020

---
+++ AssignmentProduct/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AssignmentProduct
+{
+    class Program
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-CHU7B0H\SQLEXPRESS;Initial Catalog=PracticeDB;User ID=sa;Password=pass@word1");
+        SqlCommand cmd = null;
+        public void Add(string id,string name,int price,int stock)
+        {
+            try
+            {
+                cmd = new SqlCommand("insert into product values(@id,@name,@price,@stock)", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@price", price);
+                cmd.Parameters.AddWithValue("@stock", stock);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void Get(string id)
+        {
+            try
+            {
+                cmd = new SqlCommand("select * from product where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    dr.Read();
+                    Console.WriteLine("ID:{0} Name:{1} Price:{2} Stock:{3}", dr["id"], dr["name"], dr["price"], dr["stock"]);
+                }
+                else
+                {
+                    Console.WriteLine("No products found");
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void GetAll()
+        {
+            try
+            {
+                cmd = new SqlCommand("select * from product", con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Console.WriteLine("ID:{0} Name:{1} Price:{2} Stock:{3}", dr["id"], dr["name"], dr["price"], dr["stock"]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Table empty");
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void Delete(string id)
+        {
+            try
+            {
+                cmd = new SqlCommand("delete  from product where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void Update(string id,int price,int stock)
+        {
+            try
+            {
+                cmd = new SqlCommand("update product set price=@price,stock=@stock where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@price", price);
+                cmd.Parameters.AddWithValue("@stock", stock);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            //obj.Add("P2","AC",70000,1);
+            //obj.Get("P1");
+            //obj.GetAll();
+            //obj.Delete("P2");
+            obj.Update("P1", 50000, 1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HandsOnADO/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace HandsOnADO
+{
+    class Program
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-CHU7B0H\SQLEXPRESS;Initial Catalog=PracticeDB;User ID=sa;Password=pass@word1");
+        SqlCommand cmd = null;
+        public void Add()
+        {
+            try
+            {
+                cmd = new SqlCommand("insert into project values(@pid,@pname,@sdate,@edate)", con);
+                //passing values to parameters
+                cmd.Parameters.AddWithValue("@pid", "P0006");
+                cmd.Parameters.AddWithValue("@pname", "Boing");
+                cmd.Parameters.AddWithValue("@sdate", DateTime.Parse("12.2.2019"));
+                cmd.Parameters.AddWithValue("@edate", DateTime.Parse("10.2.2022"));
+                con.Open();//open connection
+                cmd.ExecuteNonQuery();//execute query (dml)
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();//close conection
+            }
+        }
+        public void GetProjectById(string pid)
+        {
+            try 
+            {
+                cmd = new SqlCommand("select * from project where pid=@pid", con);
+                cmd.Parameters.AddWithValue("@pid", pid);
+                con.Open();
+                SqlDataReader dr=cmd.ExecuteReader();//execute select statement
+                if(dr.HasRows)//check rows existence
+                {
+                    //Read records in datareader
+                    dr.Read();//reads only one record
+                    Console.WriteLine("ID:{0} Name:{1} SDate:{2} EDate:{3}", dr["pid"], dr["pname"], dr["sdate"], dr["edate"]);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid project id");
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();//close conection
+            }
+        }
+       public void GetAllProjects()
+        {
+            try 
+            {
+                cmd = new SqlCommand("select * from project", con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    while(dr.Read())
+                    {
+                        Console.WriteLine("ID:{0} Name:{1} SDate:{2} EDate:{3}", dr["pid"], dr["pname"], dr["sdate"], dr["edate"]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Table empty");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();//close conection
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            //obj.Add();
+            //obj.GetProjectById("P0005");
+            obj.GetAllProjects();
+            Console.ReadKey();
+        }
+    }
+}
